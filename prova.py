3e23f6c0fd957599698b962505e27671afda8a6a
Soldà Federico <federicosold@outlook.com>
2015-11-03 22:39:51 +0100

    aggiunto file prova.py

---
+++ prova.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+print "Hello world"
+x=50
+for i in range(x):
+	print i
+a=range(10)
+print a
+b=["0","1","2"]
+print " ".join(b)
+c=[str(x) for x in a]
+print " ".join(c)
+
+if x>5 :
+	print "è maggiore"
